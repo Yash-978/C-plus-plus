d79dea4011e40d19188c0364fc559d72abeed99e
Yash <147479013+Yash-978@users.noreply.github.com>
2024-02-26 01:40:12 +0530

    Update password validation.cpp

--- 7. Exception Handling/Lec 7.1/password validation.cpp
+++ 7. Exception Handling/Lec 7.1/password validation.cpp
@@ -1,18 +1,27 @@
 //Q.1 Write a Program to implement exception handling mechanism for different types of scenarios:
-//- a password cannot be validated if it doesn’t contains an uppercase letter
+//- a password cannot be validated if it doesnâ€™t contains an uppercase letter
 
 #include<iostream>
+#include<string.h>
 using namespace std;
 main()
 {
 
-	int pass;
+	int l,check=0,i;
+	char pass[32];
 	cout << "Enter the Password :";
 	cin >> pass;
 	l=strlen(pass);
+	for(i=0; i<l; i++)
+	{
+		if(pass[i]>=65 && pass[i]<=90)
+		{
+			check= 1;
+		}
+	}
 	try
 	{
-		if (l>=48 ||l<=48 && l>=65 || l<=90)
+		if (check==0)
 		{
 			throw l;
 		}
@@ -26,4 +35,3 @@ main()
 		cout << "please enter valid password which contain alphanumeric variable"<< endl;
 	}
 }
-
